57076bb59ff2cd3764cb1018f57d0cf21e3b3b97
josephreddyyeruva <133141651+josephreddyyeruva@users.noreply.github.com>
2023-07-14 15:23:22 -0400

    Updated test cases for visual-properties.py

--- visual-properties.py
+++ visual-properties.py
@@ -32,3 +32,130 @@ shading_values = [
     [1.0, 1.0, 1.0, 1.0],
     [1.0, 1.0, 1.0, 1.0],
 ]
+
+#test cases:
+
+def process_test_case(test_case_number, transparent, transparency, is_cube, glass, translucent, colliders, vertex_positions, tex_coords, shading_values):
+    print(f"Test Case {test_case_number}:")
+    print("Transparent:", transparent)
+    print("Transparency:", transparency)
+    print("Is Cube:", is_cube)
+    print("Glass:", glass)
+    print("Translucent:", translucent)
+    print("Colliders:", colliders)
+    print("Vertex Positions:", vertex_positions)
+    print("Texture Coordinates:", tex_coords)
+    print("Shading Values:", shading_values)
+    print()
+
+# Test Case 1
+transparent = True
+transparency = 2
+is_cube = False
+glass = False
+translucent = False
+colliders = []
+vertex_positions = [
+    [-0.3536, 0.5000,  0.3536, -0.3536, -0.5000,  0.3536,  0.3536, -0.5000, -0.3536,  0.3536, 0.5000, -0.3536],
+    [-0.3536, 0.5000, -0.3536, -0.3536, -0.5000, -0.3536,  0.3536, -0.5000,  0.3536,  0.3536, 0.5000,  0.3536],
+    [ 0.3536, 0.5000, -0.3536,  0.3536, -0.5000, -0.3536, -0.3536, -0.5000,  0.3536, -0.3536, 0.5000,  0.3536],
+    [ 0.3536, 0.5000,  0.3536,  0.3536, -0.5000,  0.3536, -0.3536, -0.5000, -0.3536, -0.3536, 0.5000, -0.3536],
+]
+tex_coords = [
+    [0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0],
+    [0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0],
+    [0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0],
+    [0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0],
+]
+shading_values = [
+    [1.0, 1.0, 1.0, 1.0],
+    [1.0, 1.0, 1.0, 1.0],
+    [1.0, 1.0, 1.0, 1.0],
+    [1.0, 1.0, 1.0, 1.0],
+]
+
+process_test_case(1, transparent, transparency, is_cube, glass, translucent, colliders, vertex_positions, tex_coords, shading_values)
+
+# Test Case 2
+transparent = False
+transparency = 0
+is_cube = True
+glass = False
+translucent = False
+colliders = []
+vertex_positions = [
+    [-0.5, -0.5, -0.5, -0.5, 0.5, -0.5, 0.5, 0.5, -0.5, 0.5, -0.5, -0.5],
+    [-0.5, -0.5, 0.5, -0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, -0.5, 0.5],
+    [-0.5, 0.5, 0.5, -0.5, -0.5, 0.5, 0.5, -0.5, 0.5, 0.5, 0.5, 0.5],
+    [-0.5, 0.5, -0.5, -0.5, -0.5, -0.5, 0.5, -0.5, -0.5, 0.5, 0.5, -0.5],
+]
+tex_coords = [
+    [0.0, 0.0, 1.0, 0.0, 1.0, 1.0, 0.0, 1.0],
+    [0.0, 0.0, 1.0, 0.0, 1.0, 1.0, 0.0, 1.0],
+    [0.0, 0.0, 1.0, 0.0, 1.0, 1.0, 0.0, 1.0],
+    [0.0, 0.0, 1.0, 0.0, 1.0, 1.0, 0.0, 1.0],
+]
+shading_values = [
+    [1.0, 1.0, 1.0, 1.0],
+    [1.0, 1.0, 1.0, 1.0],
+    [1.0, 1.0, 1.0, 1.0],
+    [1.0, 1.0, 1.0, 1.0],
+]
+
+process_test_case(2, transparent, transparency, is_cube, glass, translucent, colliders, vertex_positions, tex_coords, shading_values)
+
+# Test Case 3
+transparent = True
+transparency = 1
+is_cube = True
+glass = True
+translucent = False
+colliders = [[-0.5, -0.5, -0.5, 0.5, 0.5, 0.5]]
+vertex_positions = [
+    [-0.5, -0.5, -0.5, -0.5, 0.5, -0.5, 0.5, 0.5, -0.5, 0.5, -0.5, -0.5],
+    [-0.5, -0.5, 0.5, -0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, -0.5, 0.5],
+    [-0.5, 0.5, 0.5, -0.5, -0.5, 0.5, 0.5, -0.5, 0.5, 0.5, 0.5, 0.5],
+    [-0.5, 0.5, -0.5, -0.5, -0.5, -0.5, 0.5, -0.5, -0.5, 0.5, 0.5, -0.5],
+]
+tex_coords = [
+    [0.0, 0.0, 1.0, 0.0, 1.0, 1.0, 0.0, 1.0],
+    [0.0, 0.0, 1.0, 0.0, 1.0, 1.0, 0.0, 1.0],
+    [0.0, 0.0, 1.0, 0.0, 1.0, 1.0, 0.0, 1.0],
+    [0.0, 0.0, 1.0, 0.0, 1.0, 1.0, 0.0, 1.0],
+]
+shading_values = [
+    [1.0, 1.0, 1.0, 1.0],
+    [1.0, 1.0, 1.0, 1.0],
+    [1.0, 1.0, 1.0, 1.0],
+    [1.0, 1.0, 1.0, 1.0],
+]
+
+process_test_case(3, transparent, transparency, is_cube, glass, translucent, colliders, vertex_positions, tex_coords, shading_values)
+
+# Test Case 4
+transparent = False
+transparency = 0
+is_cube = True
+glass = False
+translucent = True
+colliders = []
+vertex_positions = [
+    [-0.5, -0.5, -0.5, -0.5, 0.5, -0.5, 0.5, 0.5, -0.5, 0.5, -0.5, -0.5],
+    [-0.5, -0.5, 0.5, -0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, -0.5, 0.5],
+    [-0.5, 0.5, 0.5, -0.5, -0.5, 0.5, 0.5, -0.5, 0.5, 0.5, 0.5, 0.5],
+    [-0.5, 0.5, -0.5, -0.5, -0.5, -0.5, 0.5, -0.5, -0.5, 0.5, 0.5, -0.5],
+]
+tex_coords = [
+    [0.0, 0.0, 1.0, 0.0, 1.0, 1.0, 0.0, 1.0],
+    [0.0, 0.0, 1.0, 0.0, 1.0, 1.0, 0.0, 1.0],
+    [0.0, 0.0, 1.0, 0.0, 1.0, 1.0, 0.0, 1.0],
+    [0.0, 0.0, 1.0, 0.0, 1.0, 1.0, 0.0, 1.0],
+]
+shading_values = [
+    [1.0, 1.0, 1.0, 1.0],
+    [1.0, 1.0, 1.0, 1.0],
+    [1.0, 1.0, 1.0, 1.0],
+    [1.0, 1.0, 1.0, 1.0],
+]
+
+process_test_case(4, transparent, transparency, is_cube, glass, translucent, colliders, vertex_positions, tex_coords, shading_values)
