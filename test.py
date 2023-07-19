637ac2b3aa3e3c8a88999d428132728e762f70bc
josephreddyyeruva <133141651+josephreddyyeruva@users.noreply.github.com>
2023-07-19 17:17:25 -0400

    Update test cases for pygletbatchupdater.py

--- test.py
+++ test.py
@@ -387,7 +387,34 @@ class KeyboardMouseTestCase(unittest.TestCase):
 if __name__ == '__main__':
     unittest.main()
 
+"""
+Tests from pygletbatchupdater.py
+"""
+import unittest
+import pyglet
+from pyglet.graphics import Batch
+from pyglet.gl import *
 
-"""
-Errors found in Tests from pygletbatchupdater.py. Need to have ShaderProgram working before testing
-"""
+class TestUpdatedBatch(unittest.TestCase):
+    def setUp(self):
+        self.batch = UpdatedBatch()
+
+    def test_add(self):
+        """
+        Test the add method of UpdatedBatch.
+        """
+        self.batch.add(4, pyglet.gl.GL_QUADS, None, ('v2f', [0, 0, 100, 0, 100, 100, 0, 100]), ('c3B', [255, 0, 0, 0, 255, 0, 0, 0, 255, 255, 255, 0]))
+        self.assertEqual(len(self.batch.vertices), 8)
+        self.assertEqual(len(self.batch.colors), 12)
+
+    def test_add_indexed(self):
+        """
+        Test the add_indexed method of UpdatedBatch.
+        """
+        self.batch.add_indexed(4, pyglet.gl.GL_QUADS, None, [0, 1, 2, 3], ('v2f', [0, 0, 100, 0, 100, 100, 0, 100]), ('c3B', [255, 0, 0, 0, 255, 0, 0, 0, 255, 255, 255, 0]))
+        self.assertEqual(len(self.batch.vertices), 8)
+        self.assertEqual(len(self.batch.colors), 12)
+
+if __name__ == '__main__':
+    unittest.main()
+
