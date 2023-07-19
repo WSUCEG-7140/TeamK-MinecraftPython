2d6514591bcab123f4cd273bd5f7c17f7a431f66
josephreddyyeruva <133141651+josephreddyyeruva@users.noreply.github.com>
2023-07-19 15:41:02 -0400

    Updated test cases for lava.py

--- test.py
+++ test.py
@@ -169,6 +169,33 @@ class SnowBlockTestCase(unittest.TestCase):
         self.assertEqual(player.equipped_tool, shovel)  # Check if the player's equipped tool is the shovel
 
 """
+Tests from lava.py
+"""
+import unittest
+
+class SnowBlockTestCase(unittest.TestCase):
+    def test_snow_block_melts_with_lava_collision(self):
+        """
+        @brief Test case to verify that a snow block melts when it collides with a lava block.
+        """
+        snow_block = SnowBlock()
+        lava = Lava()
+        snow_block.on_collision(lava)
+        self.assertTrue(snow_block.is_melted)
+
+    def test_snow_block_does_not_melt_with_non_lava_collision(self):
+        """
+        @brief Test case to check that a snow block does not melt when colliding with a block that is not lava.
+        """
+        snow_block = SnowBlock()
+        other_block = Block()
+        snow_block.on_collision(other_block)
+        self.assertFalse(snow_block.is_melted)
+
+if __name__ == '__main__':
+    unittest.main()
+
+"""
 Errors found in Tests from keyboard_mouse.py
 """
 
