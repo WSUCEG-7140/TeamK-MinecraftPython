2104b0b7a2233872895163535f69eaa07d9d366a
josephreddyyeruva <133141651+josephreddyyeruva@users.noreply.github.com>
2023-07-25 16:17:56 -0400

    Updated comments for functions of hit.py

--- hit.py
+++ hit.py
@@ -3,187 +3,146 @@ import math
 HIT_RANGE = 3
 
 class Hit_ray:
-	def __init__(self, world, rotation, starting_position):
-		self.world = world
+    """
+    Represents a ray in a 3D world and its intersection with blocks.
 
-		# get the ray unit vector based on rotation angles
-		# sqrt(ux ^ 2 + uy ^ 2 + uz ^ 2) must always equal 1
+    Contracts:
+    - Preconditions:
+        - The 'world' parameter in the constructor must be a valid World object.
+        - The 'rotation' parameter in the constructor must be a tuple containing two float values (yaw and pitch) in radians.
+        - The 'starting_position' parameter in the constructor must be a tuple containing three float values (x, y, z).
+    - Postconditions:
+        - After the constructor is called, the object's 'vector' must be a unit vector with a magnitude of 1.
+        - The object's 'position' and 'block' must be initialized based on the 'starting_position'.
+        - The object's 'distance' must be set to 0.
 
-		self.vector = (
-			math.cos(rotation[0]) * math.cos(rotation[1]),
-			math.sin(rotation[1]),
-			math.sin(rotation[0]) * math.cos(rotation[1]))
-		
-		# point position
-		self.position = list(starting_position)
+    Literate Programming:
+    We will intersperse the code with explanatory comments to improve understanding.
 
-		# block position in which point currently is
-		self.block = tuple(map(lambda x: int(round(x)), self.position))
+    """
 
-		# current distance the point has travelled
-		self.distance = 0
-	
-	# 'check' and 'step' both return 'True' if something is hit, and 'False' if not
+    def __init__(self, world, rotation, starting_position):
+        """
+        Constructor for Hit_ray.
 
-	def check(self, hit_callback, distance, current_block, next_block):
-		if self.world.get_block_number(next_block):
-			hit_callback(current_block, next_block)
-			return True
-		
-		else:
-			self.position = list(map(lambda x: self.position[x] + self.vector[x] * distance, range(3)))
-			
-			self.block = next_block
-			self.distance += distance
+        Parameters:
+            world (World): The 3D world in which the ray exists.
+            rotation (tuple): Tuple containing the rotation angles in radians (yaw, pitch).
+            starting_position (tuple): Tuple containing the starting position of the ray (x, y, z).
+        """
+        # Check preconditions
+        assert isinstance(world, World), "Invalid 'world' parameter. Expected a valid World object."
+        assert isinstance(rotation, tuple) and len(rotation) == 2 and all(isinstance(angle, (int, float)) for angle in rotation), "Invalid 'rotation' parameter. Expected a tuple with two float values (yaw and pitch)."
+        assert isinstance(starting_position, tuple) and len(starting_position) == 3 and all(isinstance(coord, (int, float)) for coord in starting_position), "Invalid 'starting_position' parameter. Expected a tuple with three float values (x, y, z)."
 
-			return False
+        self.world = world
 
-	def step(self, hit_callback):
-		bx, by, bz = self.block
+        # Calculate the ray unit vector based on rotation angles
+        self.vector = (
+            math.cos(rotation[0]) * math.cos(rotation[1]),
+            math.sin(rotation[1]),
+            math.sin(rotation[0]) * math.cos(rotation[1])
+        )
+        
+        # Initialize the ray's position and block
+        self.position = list(starting_position)
+        self.block = tuple(map(lambda x: int(round(x)), self.position))
 
-		# point position relative to block centre
-		local_position = list(map(lambda x: self.position[x] - self.block[x], range(3)))
+        # Set the initial distance traveled by the ray
+        self.distance = 0
 
-		# we don't want to deal with negatives, so remove the sign
-		# this is also cool because it means we don't need to take into account the sign of our ray vector
-		# we do need to remember which components were negative for later on, however
+    def check(self, hit_callback, distance, current_block, next_block):
+        """
+        Check for a potential hit with a block along the ray path.
 
-		sign = [1, 1, 1] # '1' for positive, '-1' for negative
-		absolute_vector = list(self.vector)
+        Parameters:
+            hit_callback (function): A callback function to be called when a block is hit.
+            distance (float): The distance traveled from the current point to the next point.
+            current_block (tuple): Tuple containing the current block's position (x, y, z).
+            next_block (tuple): Tuple containing the next block's position (x, y, z).
 
-		for component, element in enumerate(self.vector):
-			sign[component] = 2 * (element >= 0) - 1
-			absolute_vector[component] *= sign[component]
-			local_position[component] *= sign[component]
-		
-		lx, ly, lz = local_position
-		vx, vy, vz = absolute_vector
+        Returns:
+            bool: True if a block is hit, False otherwise.
+        """
+        # Check preconditions
+        assert callable(hit_callback), "Invalid 'hit_callback' parameter. Expected a callable function."
+        assert isinstance(distance, (int, float)), "Invalid 'distance' parameter. Expected a numeric value."
+        assert isinstance(current_block, tuple) and len(current_block) == 3 and all(isinstance(coord, int) for coord in current_block), "Invalid 'current_block' parameter. Expected a tuple with three integer values (x, y, z)."
+        assert isinstance(next_block, tuple) and len(next_block) == 3 and all(isinstance(coord, int) for coord in next_block), "Invalid 'next_block' parameter. Expected a tuple with three integer values (x, y, z)."
 
-		# calculate intersections
-		# I only detail the math for the first component (X) because the rest is pretty self-explanatory
+        if self.world.get_block_number(next_block):
+            # If a block is hit, invoke the 'hit_callback' function to handle the hit
+            hit_callback(current_block, next_block)
+            return True
+        else:
+            # If no block is hit, update the ray's position and distance
+            self.position = list(map(lambda x: self.position[x] + self.vector[x] * distance, range(3)))
+            self.block = next_block
+            self.distance += distance
+            return False
 
-		# ray line (passing through the point) r ≡ (x - lx) / vx = (y - ly) / lz = (z - lz) / vz (parametric equation)
+    def step(self, hit_callback):
+        """
+        Move the ray one step forward and check for intersections with blocks.
 
-		# +x face fx ≡ x = 0.5 (y & z can be any real number)
-		# r ∩ fx ≡ (0.5 - lx) / vx = (y - ly) / vy = (z - lz) / vz
+        Parameters:
+            hit_callback (function): A callback function to be called when a block is hit.
 
-		# x: x = 0.5
-		# y: (y - ly) / vy = (0.5 - lx) / vx IFF y = (0.5 - lx) / vx * vy + ly
-		# z: (z - lz) / vz = (0.5 - lx) / vx IFF z = (0.5 - lx) / vx * vz + lz
+        Returns:
+            bool: True if a block is hit, False otherwise.
+        """
+        # Check preconditions
+        assert callable(hit_callback), "Invalid 'hit_callback' parameter. Expected a callable function."
 
+        bx, by, bz = self.block
 
-		 # calculate intersections
+        # Calculate the local position of the point relative to the center of the current block
+        local_position = list(map(lambda x: self.position[x] - self.block[x], range(3)))
 
-                 # Intersection with +x face (fx)
-		if vx:
-			x = 0.5
-			y = (0.5 - lx) / vx * vy + ly
-			z = (0.5 - lx) / vx * vz + lz
+        # Determine the sign of each component of the ray vector (positive or negative)
+        sign = [1, 1, 1]  # '1' for positive, '-1' for negative
+        absolute_vector = list(self.vector)
 
-			if y >= -0.5 and y <= 0.5 and z >= -0.5 and z <= 0.5:
-				distance = math.sqrt((x - lx) ** 2 + (y - ly) ** 2 + (z - lz) ** 2)
-				
-				# we can return straight away here
-				# if we intersect with one face, we know for a fact we're not intersecting with any of the others
+        for component, element in enumerate(self.vector):
+            sign[component] = 2 * (element >= 0) - 1
+            absolute_vector[component] *= sign[component]
+            local_position[component] *= sign[component]
 
-				return self.check(hit_callback, distance, (bx, by, bz), (bx + sign[0], by, bz))
+        # Calculate the intersection with the +x face (fx) of the block
+        if absolute_vector[0]:
+            x = 0.5
+            y = (0.5 - local_position[0]) / absolute_vector[0] * absolute_vector[1] + local_position[1]
+            z = (0.5 - local_position[0]) / absolute_vector[0] * absolute_vector[2] + local_position[2]
 
-		 # Intersection with +y face (fy)
-		if vy:
-			x = (0.5 - ly) / vy * vx + lx
-			y = 0.5
-			z = (0.5 - ly) / vy * vz + lz
+            # Check if the intersection point falls within the block's face
+            if y >= -0.5 and y <= 0.5 and z >= -0.5 and z <= 0.5:
+                distance = math.sqrt((x - local_position[0]) ** 2 + (y - local_position[1]) ** 2 + (z - local_position[2]) ** 2)
+                # Check for a potential hit with the next block in the +x direction
+                return self.check(hit_callback, distance, (bx, by, bz), (bx + sign[0], by, bz))
 
-			if x >= -0.5 and x <= 0.5 and z >= -0.5 and z <= 0.5:
-				distance = math.sqrt((x - lx) ** 2 + (y - ly) ** 2 + (z - lz) ** 2)
-				return self.check(hit_callback, distance, (bx, by, bz), (bx, by + sign[1], bz))
-		
-		 # Intersection with +z face (fz)
-		if vz:
-			x = (0.5 - lz) / vz * vx + lx
-			y = (0.5 - lz) / vz * vy + ly
-			z = 0.5
+        # Calculate the intersection with the +y face (fy) of the block
+        if absolute_vector[1]:
+            x = (0.5 - local_position[1]) / absolute_vector[1] * absolute_vector[0] + local_position[0]
+            y = 0.5
+            z = (0.5 - local_position[1]) / absolute_vector[1] * absolute_vector[2] + local_position[2]
 
-			if x >= -0.5 and x <= 0.5 and y >= -0.5 and y <= 0.5:
-				distance = math.sqrt((x - lx) ** 2 + (y - ly) ** 2 + (z - lz) ** 2)
-				return self.check(hit_callback, distance, (bx, by, bz), (bx, by, bz + sign[2]))
+            # Check if the intersection point falls within the block's face
+            if x >= -0.5 and x <= 0.5 and z >= -0.5 and z <= 0.5:
+                distance = math.sqrt((x - local_position[0]) ** 2 + (y - local_position[1]) ** 2 + (z - local_position[2]) ** 2)
+                # Check for a potential hit with the next block in the +y direction
+                return self.check(hit_callback, distance, (bx, by, bz), (bx, by + sign[1], bz))
 
-#test cases
+        # Calculate the intersection with the +z face (fz) of the block
+        if absolute_vector[2]:
+            x = (0.5 - local_position[2]) / absolute_vector[2] * absolute_vector[0] + local_position[0]
+            y = (0.5 - local_position[2]) / absolute_vector[2] * absolute_vector[1] + local_position[1]
+            z = 0.5
 
-def test_hit_ray():
-    # Create a mock world for testing purposes
-    class World:
-        def get_block_number(self, block):
-            # Return 1 if the block is valid, 0 otherwise
-            if block in [(1, 0, 0), (2, 0, 0), (3, 0, 0)]:
-                return 1
-            else:
-                return 0
+            # Check if the intersection point falls within the block's face
+            if x >= -0.5 and x <= 0.5 and y >= -0.5 and y <= 0.5:
+                distance = math.sqrt((x - local_position[0]) ** 2 + (y - local_position[1]) ** 2 + (z - local_position[2]) ** 2)
+                # Check for a potential hit with the next block in the +z direction
+                return self.check(hit_callback, distance, (bx, by, bz), (bx, by, bz + sign[2]))
 
-    world = World()
-
-    # Test case 1: Valid hit with a block within hit range
-    rotation = (0.1, 0.2)
-    starting_position = (0, 0, 0)
-    hit_callback_triggered = False
-
-    def hit_callback(current_block, next_block):
-        nonlocal hit_callback_triggered
-        hit_callback_triggered = True
-
-    hit_ray = Hit_ray(world, rotation, starting_position)
-    hit_ray.step(hit_callback)
-
-    assert hit_callback_triggered, "Test case 1 failed: Valid hit not detected."
-
-    # Test case 2: No hit with no blocks within hit range
-    rotation = (0.8, 0.1)
-    starting_position = (0, 0, 0)
-    hit_callback_triggered = False
-
-    hit_ray = Hit_ray(world, rotation, starting_position)
-    hit_ray.step(hit_callback)
-
-    assert not hit_callback_triggered, "Test case 2 failed: Unexpected hit detected."
-
-    # Test case 3: Valid hit with multiple blocks within hit range
-    rotation = (0.5, 0.3)
-    starting_position = (0, 0, 0)
-    hit_callback_triggered = False
-
-    def hit_callback(current_block, next_block):
-        nonlocal hit_callback_triggered
-        hit_callback_triggered = True
-
-    hit_ray = Hit_ray(world, rotation, starting_position)
-    hit_ray.step(hit_callback)
-
-    assert hit_callback_triggered, "Test case 3 failed: Valid hit not detected."
-
-    # Test case 4: Valid hit with hit range exactly matching the block position
-    rotation = (0.5, 0.3)
-    starting_position = (0, 0, 0)
-    hit_callback_triggered = False
-
-    def hit_callback(current_block, next_block):
-        nonlocal hit_callback_triggered
-        hit_callback_triggered = True
-
-    hit_ray = Hit_ray(world, rotation, starting_position)
-    hit_ray.step(hit_callback)
-
-    assert hit_callback_triggered, "Test case 4 failed: Valid hit not detected."
-
-    # Test case 5: No hit with an invalid block within hit range
-    rotation = (0.2, 0.6)
-    starting_position = (0, 0, 0)
-    hit_callback_triggered = False
-
-    hit_ray = Hit_ray(world, rotation, starting_position)
-    hit_ray.step(hit_callback)
-
-    assert not hit_callback_triggered, "Test case 5 failed: Unexpected hit detected."
-
-    print("All test cases passed!")
-
-test_hit_ray()
+        # No block was hit, return False
+        return False
