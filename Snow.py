c7a578c87e42a30c4374423bdc1913a0e2e08b2a
josephreddyyeruva <133141651+josephreddyyeruva@users.noreply.github.com>
2023-07-25 18:19:28 -0400

    Created and written code for Snow.py

---
+++ Snow.py
@@ -0,0 +1,176 @@
+## @class Block
+#  @brief Represents a block in the game world.
+class Block:
+    ## @brief Constructor for the Block class.
+    #  @param id The ID of the block.
+    #  @param name The name of the block.
+    #  @param texture The texture file for the block.
+    def __init__(self, id, name, texture):
+        """
+        Initializes a Block instance.
+
+        Args:
+            id (int): The ID of the block.
+            name (str): The name of the block.
+            texture (str): The texture file for the block.
+
+        Contracts:
+        - Preconditions:
+            None
+        - Postconditions:
+            - The Block instance is initialized with the provided attributes.
+        """
+        self.id = id
+        self.name = name
+        self.texture = texture
+
+## @class SnowBlock
+#  @brief Represents a snow block, which is a type of Block.
+class SnowBlock(Block):
+    ## @brief Constructor for the SnowBlock class.
+    def __init__(self):
+        """
+        Initializes a SnowBlock instance.
+
+        Contracts:
+        - Preconditions:
+            None
+        - Postconditions:
+            - The SnowBlock instance is initialized with default attributes.
+        """
+        super().__init__(id=5, name="Snow Block", texture="snow.png")
+        self.color = (255, 255, 255)  # RGB value for white color
+        self.transparent = False
+        self.cube_shape = True
+        self.colliders = [0, 1, 0, 1, 0, 1]  # Colliders for block boundaries
+        self.vertex_positions = [
+            [-0.5, -0.5, -0.5],  # Vertex positions of the cube
+            [-0.5, -0.5, 0.5],
+            [-0.5, 0.5, -0.5],
+            [-0.5, 0.5, 0.5],
+            [0.5, -0.5, -0.5],
+            [0.5, -0.5, 0.5],
+            [0.5, 0.5, -0.5],
+            [0.5, 0.5, 0.5]
+        ]
+        self.texture_coords = [
+            [0, 0],  # Texture coordinates for each vertex
+            [0, 1],
+            [1, 0],
+            [1, 1],
+            [0, 0],
+            [0, 1],
+            [1, 0],
+            [1, 1]
+        ]
+        self.shading_values = [
+            [1, 1, 1, 1]  # Shading values for each face of the cube
+        ]
+        self.is_walkable = True  # Indicates whether the snow block is walkable
+        self.slipperiness = 0.6  # Slipperiness value for the snow block
+        self.is_breakable = True  # Indicates whether the snow block can be broken
+        self.hardness = 0.2  # Hardness value for the snow block
+        self.tool_required = "shovel"  # Tool required to break the snow block
+
+    ## @brief Handles player interaction with the snow block.
+    #  @param player The player interacting with the snow block.
+    def on_player_interact(self, player):
+        """
+        Handles player interaction with the snow block.
+
+        Args:
+            player (Player): The player interacting with the snow block.
+
+        Contracts:
+        - Preconditions:
+            None
+        - Postconditions:
+            - Custom logic for player interaction with the snow block is executed.
+        """
+        # Custom logic for player interaction with the snow block
+        if player.get_equipped_tool() == "shovel":
+            self.break_block()
+
+    ## @brief Logic to break the snow block and remove it from the game world.
+    def break_block(self):
+        """
+        Logic to break the snow block and remove it from the game world.
+
+        Contracts:
+        - Preconditions:
+            None
+        - Postconditions:
+            - The snow block is broken and removed from the game world.
+        """
+        # Logic to break the snow block and remove it from the game world
+        # ...
+        pass
+
+
+## @class Player
+#  @brief Represents a player in the game.
+class Player:
+    ## @brief Constructor for the Player class.
+    def __init__(self):
+        """
+        Initializes a Player instance.
+
+        Contracts:
+        - Preconditions:
+            None
+        - Postconditions:
+            - The Player instance is initialized with an empty inventory list.
+        """
+        self.inventory = []  # Initialize an empty inventory list
+
+    ## @brief Adds an item to the player's inventory.
+    #  @param item The item to add to the inventory.
+    def add_item_to_inventory(self, item):
+        """
+        Adds an item to the player's inventory.
+
+        Args:
+            item: The item to add to the inventory.
+
+        Contracts:
+        - Preconditions:
+            None
+        - Postconditions:
+            - The item is added to the player's inventory.
+        """
+        self.inventory.append(item)
+
+
+## @class InventoryRenderer
+#  @brief Renders the player's inventory.
+class InventoryRenderer:
+    ## @brief Renders the player's inventory.
+    #  @param player The player whose inventory to render.
+    def render(self, player):
+        """
+        Renders the player's inventory.
+
+        Args:
+            player (Player): The player whose inventory to render.
+
+        Contracts:
+        - Preconditions:
+            None
+        - Postconditions:
+            - The player's inventory is rendered.
+        """
+        for item in player.inventory:
+            if isinstance(item, SnowBlock):
+                print("White Block (Snow)")
+            else:
+                print("Unknown Item")
+
+
+# Create a player instance and add the snow block to the inventory
+player = Player()
+snow_block = SnowBlock()
+player.add_item_to_inventory(snow_block)
+
+# Render the player's inventory
+inventory_renderer = InventoryRenderer()
+inventory_renderer.render(player)
