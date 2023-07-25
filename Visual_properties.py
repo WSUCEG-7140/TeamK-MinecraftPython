97254d061e4f8301427fbe3238ea1b43280a631c
josephreddyyeruva <133141651+josephreddyyeruva@users.noreply.github.com>
2023-07-25 18:08:31 -0400

    Created and written code for Visual_properties.py

---
+++ Visual_properties.py
@@ -0,0 +1,41 @@
+"""
+File: visual-properties.py
+
+Description: A  code to demonstrate object properties.
+
+"""
+
+# Set transparency-related variables
+transparent = True    # Indicates whether the object is transparent
+transparency = 2      # Transparency level (assumed to be on a scale of 0-10)
+
+# Set object properties
+is_cube = False       # Indicates whether the object is a cube
+glass = False         # Indicates whether the object is made of glass
+translucent = False   # Indicates whether the object is translucent
+
+colliders = []        # An empty list to store collider objects (if any)
+
+# Define vertex positions for the object
+vertex_positions = [
+    [-0.3536, 0.5000,  0.3536, -0.3536, -0.5000,  0.3536,  0.3536, -0.5000, -0.3536,  0.3536, 0.5000, -0.3536],
+    [-0.3536, 0.5000, -0.3536, -0.3536, -0.5000, -0.3536,  0.3536, -0.5000,  0.3536,  0.3536, 0.5000,  0.3536],
+    [ 0.3536, 0.5000, -0.3536,  0.3536, -0.5000, -0.3536, -0.3536, -0.5000,  0.3536, -0.3536, 0.5000,  0.3536],
+    [ 0.3536, 0.5000,  0.3536,  0.3536, -0.5000,  0.3536, -0.3536, -0.5000, -0.3536, -0.3536, 0.5000, -0.3536],
+]
+
+# Define texture coordinates for the object
+tex_coords = [
+    [0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0],
+    [0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0],
+    [0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0],
+    [0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0],
+]
+
+# Define shading values for the object
+shading_values = [
+    [1.0, 1.0, 1.0, 1.0],
+    [1.0, 1.0, 1.0, 1.0],
+    [1.0, 1.0, 1.0, 1.0],
+    [1.0, 1.0, 1.0, 1.0],
+]
