8d9af4eb3e294d6682f4b3874ed0169e8f4298d5
j2quik <86252538+j2quik@users.noreply.github.com>
2023-07-25 02:49:36 -0400

    Removed unnecessary variable declarations
    Global variables were used. Updated code only uses parameters passed from main or variables declared inside of functions.

--- sun.py
+++ sun.py
@@ -4,19 +4,9 @@ from math import sin, cos, pi
 
 
 """
-Sun settings
+Create vertex data for a simple textured sphere
 """
-SUN_RADIUS = 3
-SUN_POSITION = (10, 10, 10)
-
-    
-"""
-Vertex data for a simple textured sphere
-"""
-sphere_vertex_list = pyglet.graphics.vertex_list(0, ('v3f', []))
-
-
-def create_sphere(radius, slices, stacks):
+def create_sun(radius, slices, stacks):
     vertices = []
     for stack in range(stacks + 1):
         for slice in range(slices):
@@ -24,39 +14,30 @@ def create_sphere(radius, slices, stacks):
             y = radius * sin(-pi / 2 + pi * stack / stacks)
             z = radius * sin(2 * pi * slice / slices) * sin(pi * stack / stacks)
             vertices.extend([x, y, z])
-    return pyglet.graphics.vertex_list(len(vertices) // 3, ('v3f', vertices))
+    draw(pyglet.graphics.vertex_list(len(vertices) // 3, ('v3f', vertices)))
 
 
-def update(dt):
-    pass
+"""
+Enable buffer to draw
+"""
+def draw(sphere_vertex_list):
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+    glLoadIdentity()
+    draw_sun(sphere_vertex_list)
+    
 
+"""
+Draw the sun
+"""
+def draw_sun(sphere_vertex_list):
+    SUN_POSITION = (10, 10, 10)
 
-def draw_sun():
     glPushMatrix()
     glTranslatef(*SUN_POSITION)
     sphere_vertex_list.draw(GL_POINTS)
     glPopMatrix()
 
 
-def draw():
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-    glLoadIdentity()
-
-
-    """
-    Draw the sun
-    """
-    draw_sun()
-
-
-if __name__ == "__main__":
-    
-    # Load a texture for the sun (you can use your own texture)
-    #sun_texture = pyglet.image.load('path/to/your/sun_texture.png').get_texture()
-
-    """
-    Set up the sun's vertex data
-    """
-    sphere_vertex_list = create_sphere(SUN_RADIUS, slices=30, stacks=30)
-    draw()
+def update(dt):
+    pass
 
