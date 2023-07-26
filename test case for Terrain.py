e6b3aac65ab8147eed31da1bbc29581fbf6649da
josephreddyyeruva <133141651+josephreddyyeruva@users.noreply.github.com>
2023-07-26 00:37:25 -0400

    Updated test cases for Terrain.py

--- test case for Terrain.py
+++ test case for Terrain.py
@@ -3,6 +3,68 @@ Tests from terrain.py
 """
 import unittest
 
+class TestTerrain(unittest.TestCase):
+    """
+    Test case for the terrain functions.
+    """
+
+    def test_add_water_single_block(self):
+        """
+        Test adding a single water block to the terrain.
+        """
+        terrain = create_empty_terrain(10, 5, 10)
+        add_water(terrain, 3, 1, 3, 1, 1, 1)
+        self.assertEqual(terrain[3][1][3], "blue")
+
+    def test_add_water_multiple_blocks(self):
+        """
+        Test adding multiple water blocks to the terrain.
+        """
+        terrain = create_empty_terrain(10, 5, 10)
+        add_water(terrain, 2, 0, 2, 3, 3, 3)
+        for x in range(2, 5):
+            for y in range(0, 3):
+                for z in range(2, 5):
+                    self.assertEqual(terrain[x][y][z], "blue")
+
+    def test_print_terrain(self):
+        """
+        Test printing the terrain.
+        """
+        terrain = create_empty_terrain(10, 5, 10)
+        # Add some water to the terrain
+        add_water(terrain, 2, 0, 2, 3, 3, 3)
+        try:
+            # Capture the printed output using StringIO
+            from io import StringIO
+            import sys
+
+            captured_output = StringIO()
+            sys.stdout = captured_output
+
+            # Call the function that prints the terrain
+            print_terrain(terrain)
+
+            # Reset stdout
+            sys.stdout = sys.__stdout__
+
+            # Validate the printed output
+            expected_output = (
+                "        \n"
+                "        \n"
+                "        \n"
+                "  blue blue blue     \n"
+                "  blue blue blue     \n"
+                "  blue blue blue     \n"
+                "        \n"
+                "        \n"
+                "        \n"
+                "        \n"
+            )
+            self.assertEqual(captured_output.getvalue(), expected_output)
+        except Exception as e:
+            self.fail(f"Printing the terrain failed with exception: {e}")
+
 # Function to create an empty terrain
 def create_empty_terrain(width, height, depth):
     """
