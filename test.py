0ee9daac41a4f478aeba6a1650481018184adb6a
j2quik <86252538+j2quik@users.noreply.github.com>
2023-07-26 00:34:32 -0400

    Added test cases for moon.py
    Forgot to add the test cases with the original pull request associated with Issue#28.

--- test.py
+++ test.py
@@ -690,10 +690,9 @@ def create_cloud(radius, slices, stacks):
 
 
     """
-    Pass: if the list vertices is not empty
+    Pass: if list vertices is not empty
     """ 
     assert vertices is not None, 'vertices is empty!'
-
 
     create_clouds(pyglet.graphics.vertex_list(len(vertices) // 3, ('v3f', vertices)))
 
@@ -704,7 +703,7 @@ Test: create cloud positions
 def create_clouds(sphere_vertex_list):
 
     """
-    Pass: if the list sphere_vertex_list is not empty
+    Pass: if list sphere_vertex_list is not empty
     """ 
     assert sphere_vertex_list is not None, 'sphere_vertex_list is empty!'
 
@@ -714,7 +713,7 @@ def create_clouds(sphere_vertex_list):
 
 
     """
-    Pass: if the path to CLOUD_TEXTURE_PATH is not empty
+    Pass: if path to CLOUD_TEXTURE_PATH is not empty
     """ 
     assert CLOUD_TEXTURE_PATH is not None , 'CLOUD_TEXTURE_PATH is empty!'
 
@@ -747,6 +746,15 @@ def create_clouds(sphere_vertex_list):
     """ 
     assert clouds is not None, 'clouds is empty!'
 
+
+    """
+    Pass: if each element in list clouds is numerical
+    """ 
+    for coordinates in clouds:
+        for coordinate in coordinates:
+            coordinate_float = float(coordinate)
+            assert isinstance(coordinate_float, float), 'clouds contains a non-numerical value!'
+
     draw(sphere_vertex_list, clouds, cloud_texture)
 
 
@@ -759,7 +767,7 @@ def draw(sphere_vertex_list, clouds, cloud_texture):
 
 
     """
-    Pass: if the list sphere_vertex_list is not empty
+    Pass: if list sphere_vertex_list is not empty
     """ 
     assert sphere_vertex_list is not None, 'sphere_vertex_list is empty!'
 
@@ -784,7 +792,7 @@ Test: draw the clouds
 def draw_clouds(sphere_vertex_list, clouds, cloud_texture):
 
     """
-    Pass: if the list sphere_vertex_list is not empty
+    Pass: if list sphere_vertex_list is not empty
     """ 
     assert sphere_vertex_list is not None, 'sphere_vertex_list is empty!'
 
@@ -799,6 +807,7 @@ def draw_clouds(sphere_vertex_list, clouds, cloud_texture):
     Pass: if cloud_texture is not empty
     """ 
     assert cloud_texture is not None, 'cloud_texture is empty!'
+
     glBindTexture(GL_TEXTURE_2D, cloud_texture.id)
     glColor4f(1, 1, 1, 1)
 
@@ -808,3 +817,181 @@ def draw_clouds(sphere_vertex_list, clouds, cloud_texture):
         sphere_vertex_list.draw(GL_POINTS)
         glPopMatrix()
 
+
+
+
+"""
+TESTS for sun.py
+"""
+
+
+"""
+Test: create vertex data for a simple textured sphere
+"""
+def create_sun(radius, slices, stacks):
+
+    """
+    Pass: if radius is > 0
+    """
+    assert radius > 0, 'radius is not greater than 0!'
+
+
+    """
+    Pass: if slices is > 0
+    """
+    assert slices > 0, 'slices is not greater than 0!'
+
+
+    """
+    Pass: if stacks is > 0
+    """
+    assert stacks > 0, 'stacks is not greater than 0!'
+    
+    vertices = []
+    
+    for stack in range(stacks + 1):
+        for slice in range(slices):
+            x = radius * cos(2 * pi * slice / slices) * sin(pi * stack / stacks)
+            y = radius * sin(-pi / 2 + pi * stack / stacks)
+            z = radius * sin(2 * pi * slice / slices) * sin(pi * stack / stacks)
+            vertices.extend([x, y, z])
+
+
+    """
+    Pass: if list vertices is not empty
+    """ 
+    assert vertices is not None, 'vertices is empty!'
+
+
+    """
+    Pass: if each element in list vertices is numerical
+    """ 
+    for coordinates in vertices:
+        for coordinate in coordinates:
+            coordinate_float = float(coordinate)
+            assert isinstance(coordinate_float, float), 'vertices contains a non-numerical value!'
+            
+
+    draw(pyglet.graphics.vertex_list(len(vertices) // 3, ('v3f', vertices)))
+
+
+"""
+Test: enable buffer to draw
+"""
+def draw(sphere_vertex_list):
+
+    """
+    Pass: if list sphere_vertex_list is not empty
+    """ 
+    assert sphere_vertex_list is not None, 'sphere_vertex_list is empty!'
+
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+    glLoadIdentity()
+    draw_sun(sphere_vertex_list)
+    
+
+"""
+Test: draw the sun
+"""
+def draw_sun(sphere_vertex_list):
+
+    """
+    Pass: if list sphere_vertex_list is not empty
+    """ 
+    assert sphere_vertex_list is not None, 'sphere_vertex_list is empty!'
+
+    SUN_POSITION = (10, 10, 10)
+
+    glPushMatrix()
+    glTranslatef(*SUN_POSITION)
+    sphere_vertex_list.draw(GL_POINTS)
+    glPopMatrix()
+
+
+
+
+"""
+TESTS for moon.py
+"""
+
+
+"""
+Test: create vertex data for a simple textured sphere
+"""
+def create_moon(radius, slices, stacks):
+
+    """
+    Pass: if radius is > 0
+    """
+    assert radius > 0, 'radius is not greater than 0!'
+
+
+    """
+    Pass: if slices is > 0
+    """
+    assert slices > 0, 'slices is not greater than 0!'
+
+
+    """
+    Pass: if stacks is > 0
+    """
+    assert stacks > 0, 'stacks is not greater than 0!'
+    
+    vertices = []
+
+    for stack in range(stacks + 1):
+        for slice in range(slices):
+            x = radius * cos(2 * pi * slice / slices) * sin(pi * stack / stacks)
+            y = radius * sin(-pi / 2 + pi * stack / stacks)
+            z = radius * sin(2 * pi * slice / slices) * sin(pi * stack / stacks)
+            vertices.extend([x, y, z])
+
+
+    """
+    Pass: if list vertices is not empty
+    """ 
+    assert vertices is not None, 'vertices is empty!'
+
+
+    """
+    Pass: if each element in list vertices is numerical
+    """ 
+    for coordinates in vertices:
+        for coordinate in coordinates:
+            coordinate_float = float(coordinate)
+            assert isinstance(coordinate_float, float), 'vertices contains a non-numerical value!'
+
+    draw(pyglet.graphics.vertex_list(len(vertices) // 3, ('v3f', vertices)))
+
+
+"""
+Test: enable buffer to draw
+"""
+def draw(sphere_vertex_list):
+
+    """
+    Pass: if list sphere_vertex_list is not empty
+    """ 
+    assert sphere_vertex_list is not None, 'sphere_vertex_list is empty!'
+
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+    glLoadIdentity()
+    draw_moon(sphere_vertex_list)
+
+
+"""
+Test: draw the moon
+"""
+def draw_moon(sphere_vertex_list):
+
+    """
+    Pass: if list sphere_vertex_list is not empty
+    """ 
+    assert sphere_vertex_list is not None, 'sphere_vertex_list is empty!'
+    
+    MOON_POSITION = (0, 15, -15)
+
+    glPushMatrix()
+    glTranslatef(*MOON_POSITION)
+    sphere_vertex_list.draw(GL_POINTS)
+    glPopMatrix()
