21f1318d0e43fcce63f88f883361283a236f9e7d
josephreddyyeruva <133141651+josephreddyyeruva@users.noreply.github.com>
2023-07-26 20:53:49 -0400

    Updated doxygen tags (@returns, @param[in], and @param[out]) in comments for Visual_properties.py

--- Visual_properties.py
+++ Visual_properties.py
@@ -4,17 +4,16 @@ File: Visual-properties.py
 Description: A  code to demonstrate object properties.
 
 """
-
 # Set transparency-related variables
-transparent = True    # Indicates whether the object is transparent
-transparency = 2      # Transparency level (assumed to be on a scale of 0-10)
+transparent = True    # Indicates whether the object is transparent  /**< @param[in] transparent: boolean flag indicating if the object is transparent */
+transparency = 2      # Transparency level (assumed to be on a scale of 0-10)  /**< @param[in] transparency: integer representing the transparency level on a scale of 0-10 */
 
 # Set object properties
-is_cube = False       # Indicates whether the object is a cube
-glass = False         # Indicates whether the object is made of glass
-translucent = False   # Indicates whether the object is translucent
+is_cube = False       # Indicates whether the object is a cube  /**< @param[in] is_cube: boolean flag indicating if the object is a cube */
+glass = False         # Indicates whether the object is made of glass  /**< @param[in] glass: boolean flag indicating if the object is made of glass */
+translucent = False   # Indicates whether the object is translucent  /**< @param[in] translucent: boolean flag indicating if the object is translucent */
 
-colliders = []        # An empty list to store collider objects (if any)
+colliders = []        # An empty list to store collider objects (if any)  /**< @param[out] colliders: list to store collider objects */
 
 # Define vertex positions for the object
 vertex_positions = [
